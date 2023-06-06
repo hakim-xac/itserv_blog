704f123565ab3cb3a7eaec9f5f4edd4929274254
hakim-xac <hakim.xac@gmail.com>
2023-06-06 23:49:20 +0700

    Fixed a bug with the address to the main page in the file blog / urls .py

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 
 urlpatterns = [    
     path('', views.main_page, name='index'),
-    path(r'^blog/', views.main_page, name='index'),
+    path(r'blog/', views.main_page, name='index'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
